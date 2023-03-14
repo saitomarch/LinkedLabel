6670e6438c947a0efd32a88f95f55d0ccfaa53d5
SAITO Tomomi <t-saito@project-flora.net>
2023-03-14 10:09:38 +0900

    Initial Commit

---
+++ LinkedLabel/LinkedLabel.h
@@ -0,0 +1,18 @@
+//
+//  LinkedLabel.h
+//  LinkedLabel
+//
+//  Created by SAITO Tomomi on 2023/03/14.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LinkedLabel.
+FOUNDATION_EXPORT double LinkedLabelVersionNumber;
+
+//! Project version string for LinkedLabel.
+FOUNDATION_EXPORT const unsigned char LinkedLabelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LinkedLabel/PublicHeader.h>
+
+
